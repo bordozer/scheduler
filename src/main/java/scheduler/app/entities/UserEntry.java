eb6fae15b76b9419aee5223f3d1901ab33c94997
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-05 13:05:47 +0200

    Rethinking Architecture 7.1

--- src/main/java/scheduler/app/entities/UserEntry.java
+++ src/main/java/scheduler/app/entities/UserEntry.java
@@ -9,12 +9,6 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "T_USER")
-/*@NamedQueries( {
-		@NamedQuery(
-				name = NAMED_QUERY_FIND_BY_LOGIN,
-				query = "select u from UserEntry u inner join u.secureDetails sd where u.secureDetails.login = :login"
-		)
-} )*/
 @org.hibernate.annotations.Cache(region = "common", usage = CacheConcurrencyStrategy.READ_WRITE)
 @Getter
 @Setter
@@ -34,33 +28,4 @@ public class UserEntry implements DBEntity {
 
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private UserSecureDetailsEntry secureDetails;
-
-    /*@Override
-	public int hashCode() {
-        return (int) (31 * id);
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-
-        if (obj == null) {
-            return false;
-        }
-
-        if (obj == this) {
-            return true;
-        }
-
-        if (!(obj instanceof UserEntry)) {
-            return false;
-        }
-
-        final UserEntry userEntry = (UserEntry) obj;
-        return userEntry.getId().equals((id));
-    }
-
-    @Override
-    public String toString() {
-        return String.format("#%d: %s ( %s )", getId(), login, username);
-    }*/
 }
