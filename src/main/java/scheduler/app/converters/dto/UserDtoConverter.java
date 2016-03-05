d8ee1c7144da5f1125c7411c8f36e1af9ef3b089
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-05 19:32:23 +0200

    Scheduler task edit 21.3

--- src/main/java/scheduler/app/converters/dto/UserDtoConverter.java
+++
@@ -1,8 +0,0 @@
-package scheduler.app.converters.dto;
-
-import scheduler.app.dto.UserDto;
-import scheduler.app.models.User;
-
-public interface UserDtoConverter extends GenericDtoConverter<User, UserDto> {
-
-}
