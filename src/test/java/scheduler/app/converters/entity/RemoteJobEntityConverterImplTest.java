ad7affbcb953a602275da76103735b7f5595a496
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-06 12:56:29 +0200

    Test data utils

---
+++ src/test/java/scheduler/app/converters/entity/RemoteJobEntityConverterImplTest.java
@@ -0,0 +1,56 @@
+package scheduler.app.converters.entity;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.springframework.web.bind.annotation.RequestMethod;
+import scheduler.app.entities.RemoteJobEntity;
+import scheduler.app.models.RemoteJob;
+import scheduler.app.utils.TestEntitiesUtils;
+
+import static org.junit.Assert.assertEquals;
+
+public class RemoteJobEntityConverterImplTest {
+
+	private static final Long REMOTE_JOB_ID = 111L;
+	private static final String REQUEST_URL = "nba.com";
+	private static final RequestMethod REQUEST_METHOD = RequestMethod.POST;
+	private static final String AUTH_STRING = "ABCDE";
+	private static final String POST_JSON = "{value:111}";
+
+	@InjectMocks
+	private RemoteJobEntityConverterImpl sut = new RemoteJobEntityConverterImpl();
+
+	@Test
+	public void shouldPopulateEntity() {
+		final RemoteJobEntity entity = new RemoteJobEntity();
+		sut.populateEntity(entity, constructModel());
+
+		assertEquals(REMOTE_JOB_ID, entity.getId());
+		assertEquals(REQUEST_URL, entity.getRequestUrl());
+		assertEquals(REQUEST_METHOD, entity.getRequestMethod());
+		assertEquals(AUTH_STRING, entity.getAuthString());
+		assertEquals(POST_JSON, entity.getPostJson());
+	}
+
+	@Test
+	public void shouldConvertEntityToModel() {
+
+		final RemoteJob model = sut.toModel(TestEntitiesUtils.constructRemoteJobEntity(TestEntitiesUtils.constructSchedulerTaskEntity(user)));
+
+		assertEquals(REMOTE_JOB_ID, model.getId());
+		assertEquals(REQUEST_URL, model.getRequestUrl());
+		assertEquals(REQUEST_METHOD, model.getRequestMethod());
+		assertEquals(AUTH_STRING, model.getAuthString());
+		assertEquals(POST_JSON, model.getPostJson());
+	}
+
+	private RemoteJob constructModel() {
+		final RemoteJob model = new RemoteJob();
+		model.setId(REMOTE_JOB_ID);
+		model.setRequestUrl(REQUEST_URL);
+		model.setRequestMethod(REQUEST_METHOD);
+		model.setAuthString(AUTH_STRING);
+		model.setPostJson(POST_JSON);
+		return model;
+	}
+}
---
+++ src/test/java/scheduler/app/utils/TestEntitiesUtils.java
@@ -0,0 +1,81 @@
+package scheduler.app.utils;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import scheduler.app.entities.RemoteJobEntity;
+import scheduler.app.entities.SchedulerTaskEntity;
+import scheduler.app.entities.UserEntity;
+import scheduler.app.entities.UserSecureDetailsEntity;
+import scheduler.app.models.SchedulerTaskType;
+import scheduler.app.models.UserRole;
+
+public class TestEntitiesUtils {
+
+    public static final Long USER_ID = 33L;
+    public static final String USER_NAME = "User name";
+
+    public static final Long USER_SECURE_DETAILS_ID = 55L;
+    public static final String USER_LOGIN = "login";
+    public static final String USER_PASSWORD = "password";
+    public static final UserRole USER_ROLE = UserRole.USER;
+
+    public static final Long SCHEDULER_TASK_ID = 222L;
+    public static final String SCHEDULER_TASK_NAME = "Scheduler task name";
+    public static final SchedulerTaskType SCHEDULER_TASK_TYPE = SchedulerTaskType.DAILY;
+    public static final String SCHEDULER_TASK_DESCRIPTION = "Scheduler task description";
+    public static final String SCHEDULER_TASK_PARAMETERS_JSON = "{id:12, value:44}";
+
+    public static final Long REMOTE_JOB_ID = 111L;
+    public static final String REQUEST_URL = "nba.com";
+    public static final RequestMethod REQUEST_METHOD = RequestMethod.POST;
+    public static final String AUTH_STRING = "ABCDE";
+    public static final String POST_JSON = "{value:111}";
+
+    public static UserEntity constructUserEntity() {
+        final UserEntity user = new UserEntity();
+        user.setId(USER_ID);
+        user.setUsername(USER_NAME);
+
+        user.setSecureDetails(constructUserSecureDetailsEntity(user));
+
+        return user;
+    }
+
+    public static UserSecureDetailsEntity constructUserSecureDetailsEntity(final UserEntity user) {
+        final UserSecureDetailsEntity secureDetails = new UserSecureDetailsEntity();
+        secureDetails.setId(USER_SECURE_DETAILS_ID);
+        secureDetails.setUser(user);
+        secureDetails.setLogin(USER_LOGIN);
+        secureDetails.setPassword(USER_PASSWORD);
+        secureDetails.setRole(USER_ROLE);
+
+        user.setSecureDetails(secureDetails);
+
+        return secureDetails;
+    }
+
+    public static SchedulerTaskEntity constructSchedulerTaskEntity(final UserEntity user) {
+        final SchedulerTaskEntity schedulerTask = new SchedulerTaskEntity();
+        schedulerTask.setId(SCHEDULER_TASK_ID);
+        schedulerTask.setUser(user);
+        schedulerTask.setTaskName(SCHEDULER_TASK_NAME);
+        schedulerTask.setTaskDescription(SCHEDULER_TASK_DESCRIPTION);
+        schedulerTask.setTaskType(SCHEDULER_TASK_TYPE);
+        schedulerTask.setTaskParametersJSON(SCHEDULER_TASK_PARAMETERS_JSON);
+        return schedulerTask;
+    }
+
+    public static RemoteJobEntity constructRemoteJobEntity(SchedulerTaskEntity schedulerTaskEntity) {
+        final RemoteJobEntity remoteJobEntity = new RemoteJobEntity();
+        remoteJobEntity.setId(REMOTE_JOB_ID);
+        remoteJobEntity.setRequestUrl(REQUEST_URL);
+        remoteJobEntity.setRequestMethod(REQUEST_METHOD);
+        remoteJobEntity.setAuthString(AUTH_STRING);
+        remoteJobEntity.setPostJson(POST_JSON);
+        remoteJobEntity.setSchedulerTask(schedulerTaskEntity);
+
+        schedulerTaskEntity.setRemoteJob(remoteJobEntity);
+
+        return remoteJobEntity;
+    }
+
+}
