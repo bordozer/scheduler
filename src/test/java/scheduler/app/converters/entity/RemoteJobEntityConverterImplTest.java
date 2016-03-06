70431ec8cd99bc32e2f060b66f56ff93927bb9d6
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-06 13:27:18 +0200

    Test data utils 4

--- src/test/java/scheduler/app/converters/entity/RemoteJobEntityConverterImplTest.java
+++ src/test/java/scheduler/app/converters/entity/RemoteJobEntityConverterImplTest.java
@@ -12,29 +12,29 @@ import static org.junit.Assert.assertEquals;
 public class RemoteJobEntityConverterImplTest {
 
     @InjectMocks
-	private RemoteJobEntityConverterImpl sut = new RemoteJobEntityConverterImpl();
+    private RemoteJobEntityConverterImpl sut = new RemoteJobEntityConverterImpl();
 
-	@Test
-	public void shouldPopulateEntity() {
-		final RemoteJobEntity entity = new RemoteJobEntity();
-		sut.populateEntity(entity, TestDataModels.remoteJob());
+    @Test
+    public void shouldPopulateEntity() {
+        final RemoteJobEntity entity = new RemoteJobEntity();
+        sut.populateEntity( entity, TestDataModels.remoteJob() );
 
-		assertEquals(TestDataModels.REMOTE_JOB_ID, entity.getId());
-		assertEquals(TestDataModels.REQUEST_URL, entity.getRequestUrl());
-		assertEquals(TestDataModels.REQUEST_METHOD, entity.getRequestMethod());
-		assertEquals(TestDataModels.AUTH_STRING, entity.getAuthString());
-		assertEquals(TestDataModels.POST_JSON, entity.getPostJson());
-	}
+        assertEquals( TestDataModels.REMOTE_JOB_ID, entity.getId() );
+        assertEquals( TestDataModels.REQUEST_URL, entity.getRequestUrl() );
+        assertEquals( TestDataModels.REQUEST_METHOD, entity.getRequestMethod() );
+        assertEquals( TestDataModels.AUTH_STRING, entity.getAuthString() );
+        assertEquals( TestDataModels.POST_JSON, entity.getPostJson() );
+    }
 
-	@Test
-	public void shouldConvertEntityToModel() {
-        final RemoteJob model = sut.toModel(TestDataEntities.remoteJob());
+    @Test
+    public void shouldConvertEntityToModel() {
+        final RemoteJob model = sut.toModel( TestDataEntities.remoteJob() );
 
-		assertEquals(TestDataModels.REMOTE_JOB_ID, model.getId());
-		assertEquals(TestDataModels.REQUEST_URL, model.getRequestUrl());
-		assertEquals(TestDataModels.REQUEST_METHOD, model.getRequestMethod());
-		assertEquals(TestDataModels.AUTH_STRING, model.getAuthString());
-		assertEquals(TestDataModels.POST_JSON, model.getPostJson());
-	}
+        assertEquals( TestDataModels.REMOTE_JOB_ID, model.getId() );
+        assertEquals( TestDataModels.REQUEST_URL, model.getRequestUrl() );
+        assertEquals( TestDataModels.REQUEST_METHOD, model.getRequestMethod() );
+        assertEquals( TestDataModels.AUTH_STRING, model.getAuthString() );
+        assertEquals( TestDataModels.POST_JSON, model.getPostJson() );
+    }
 
 }
