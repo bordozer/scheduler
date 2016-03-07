ae24837d614c604c5bebafc967c63eda8a9158be
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-07 13:55:58 +0200

    Scheduler rest tests - first test 7

--- scheduler/src/main/java/scheduler/app/controllers/rest/users/edit/UserEditRestController.java
+++ scheduler/src/main/java/scheduler/app/controllers/rest/users/edit/UserEditRestController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import scheduler.app.dto.UserDto;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/rest/users")
 public class UserEditRestController {
 
     @RequestMapping(method = RequestMethod.PUT, value = "/register/")
-    public RegistrationResponse create(final @RequestBody UserEditDto editDTO) {
+    public RegistrationResponse create(final @Valid @RequestBody UserEditDto editDTO) {
         UserDto user = new UserDto();
         user.setUserId(111L);
         user.setUserName(editDTO.getName());
