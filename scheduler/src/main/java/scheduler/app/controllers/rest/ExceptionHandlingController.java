a43543edbf4eaabbc55eebc8ffeb026ae3711852
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-07 16:58:55 +0200

    User register 6

---
+++ scheduler/src/main/java/scheduler/app/controllers/rest/ExceptionHandlingController.java
@@ -0,0 +1,36 @@
+package scheduler.app.controllers.rest;
+
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import scheduler.app.esceptions.FieldErrorResource;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@ControllerAdvice
+public class ExceptionHandlingController {
+
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public void handleException(final Exception e, final Writer writer) throws IOException {
+        List<FieldError> fieldErrors = ((MethodArgumentNotValidException) e).getBindingResult().getFieldErrors();
+        List<FieldErrorResource> response = fieldErrors.stream()
+                .map(fieldError -> new FieldErrorResource(
+                        fieldError.getField(),
+                        fieldError.getRejectedValue().toString(),
+                        fieldError.getDefaultMessage()))
+                .collect(Collectors.toList()
+                );
+        writer.write(new Gson().toJson(response));
+    }
+}
--- scheduler/src/main/java/scheduler/app/controllers/rest/users/edit/UserEditRestController.java
+++ scheduler/src/main/java/scheduler/app/controllers/rest/users/edit/UserEditRestController.java
@@ -1,24 +1,11 @@
 package scheduler.app.controllers.rest.users.edit;
 
-import com.google.gson.Gson;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import scheduler.app.esceptions.BackendException;
 import scheduler.app.dto.UserDto;
-import scheduler.app.esceptions.FieldErrorResource;
-
-import java.io.IOException;
-import java.io.Writer;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/rest/users")
@@ -33,20 +20,5 @@ public class UserEditRestController {
         RegistrationResponse registrationResponse = new RegistrationResponse(true);
         registrationResponse.setUser(user);
         return registrationResponse;
-    }
-
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public void handleException(final Exception e, final Writer writer) throws IOException {
-        List<FieldError> fieldErrors = ((MethodArgumentNotValidException) e).getBindingResult().getFieldErrors();
-        List<FieldErrorResource> response = fieldErrors.stream()
-                .map(fieldError -> new FieldErrorResource(
-                        fieldError.getField(),
-                        fieldError.getRejectedValue().toString(),
-                        fieldError.getDefaultMessage()))
-                .collect(Collectors.toList()
-                );
-        writer.write(new Gson().toJson(response));
-//        writer.write(new Gson().toJson(((MethodArgumentNotValidException) e).getBindingResult().getAllErrors()));
     }
 }
