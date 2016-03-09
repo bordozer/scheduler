faa27e107e466db41e41b27733568ab7f8136efa
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-09 16:24:30 +0200

    Spring security login 2

--- scheduler/src/main/java/scheduler/app/security/SecurityUserDetailsService.java
+++ scheduler/src/main/java/scheduler/app/security/SecurityUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 import scheduler.app.entities.UserEntity;
 import scheduler.app.repositories.UserRepository;
 
