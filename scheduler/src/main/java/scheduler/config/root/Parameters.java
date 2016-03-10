177946d44c67848578854c416dad4ff7cc158ff7
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-10 17:18:27 +0200

    Security rethinking: 7

--- scheduler/src/main/java/scheduler/config/root/Parameters.java
+++ scheduler/src/main/java/scheduler/config/root/Parameters.java
@@ -2,7 +2,7 @@ package scheduler.config.root;
 
 public class Parameters {
 
-    public static final String LOGIN_PAGE_URL = "/resources/public/login/login.html";
+    public static final String LOGIN_PAGE_URL = "/resources/login.html";
     static final String LOGIN_END_POINT = "/authenticate";
     public static final String USERNAME = "login";
     public static final String PASSWORD = "password";
