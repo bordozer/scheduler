3e755ada6994fb54f913e5c16b8b3256d440768b
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-10 12:43:26 +0200

    Spring security login REST 2

--- rest-tests/src/test/java/scheduler/rest/users/UserLogin.java
+++ rest-tests/src/test/java/scheduler/rest/users/UserLogin.java
@@ -13,29 +13,13 @@ public class UserLogin {
     @Test
     public void shouldLoginUser() {
         UserData userData = DataGenerator.generateUserData();
-
         Response taskListResponse1 = RestTestHelper.doGet(SchedulerTaskRoutes.SCHEDULER_TASK_LIST, HttpStatus.SC_UNAUTHORIZED);
-//        assertEquals(HttpStatus.SC_UNAUTHORIZED, taskListResponse1.getStatusCode());
-
         Response loginResponse1 = RestTestHelper.login(userData.getLogin(), userData.getPassword(), HttpStatus.SC_UNAUTHORIZED);
-//        assertEquals(HttpStatus.SC_UNAUTHORIZED, loginResponse1.getStatusCode());
-
         Response registerResponse = RestTestHelper.register(userData.getLogin(), userData.getPassword(), userData.getName());
-//        assertEquals(HttpStatus.SC_OK, registerResponse.getStatusCode());
-
         Response taskListResponse2 = RestTestHelper.doGet(SchedulerTaskRoutes.SCHEDULER_TASK_LIST, HttpStatus.SC_UNAUTHORIZED);
-//        assertEquals(HttpStatus.SC_UNAUTHORIZED, taskListResponse2.getStatusCode());
-
         Response loginResponse2 = RestTestHelper.login(userData.getLogin(), userData.getPassword(), HttpStatus.SC_OK);
-//        assertEquals(HttpStatus.SC_OK, loginResponse2.getStatusCode());
-
         Response taskListResponse3 = RestTestHelper.doGet(SchedulerTaskRoutes.SCHEDULER_TASK_LIST, HttpStatus.SC_OK);
-//        assertEquals(HttpStatus.SC_OK, taskListResponse3.getStatusCode());
-
         Response logoutResponse = RestTestHelper.logout();
-//        assertEquals(HttpStatus.SC_OK, logoutResponse.getStatusCode());
-
         Response taskListResponse4 = RestTestHelper.doGet(SchedulerTaskRoutes.SCHEDULER_TASK_LIST, HttpStatus.SC_UNAUTHORIZED);
-//        assertEquals(HttpStatus.SC_UNAUTHORIZED, taskListResponse4.getStatusCode());
     }
 }
--- scheduler/src/main/java/scheduler/app/security/HttpAuthenticationEntryPoint.java
+++ scheduler/src/main/java/scheduler/app/security/HttpAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package scheduler.app.security;
 
+import com.google.gson.Gson;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -8,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class HttpAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -15,6 +17,15 @@ public class HttpAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(final HttpServletRequest request, final HttpServletResponse response,
                          final AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.addParameters("auth_result", "Unauthorized");
+        authResponse.addParameters("error", authException.getMessage());
+
+        PrintWriter writer = response.getWriter();
+        writer.write(new Gson().toJson(authResponse));
+        writer.flush();
     }
 }
