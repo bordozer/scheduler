95ba1d5b66dac61597735ec3463020361e4d4bf2
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-11 18:12:15 +0200

    Scheduler task edit 8

--- scheduler/src/main/java/scheduler/app/controllers/rest/scheduler/tasks/item/edit/dto/RemoteJobEditDto.java
+++ scheduler/src/main/java/scheduler/app/controllers/rest/scheduler/tasks/item/edit/dto/RemoteJobEditDto.java
@@ -3,17 +3,27 @@ package scheduler.app.controllers.rest.scheduler.tasks.item.edit.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.NotBlank;
 import scheduler.app.dto.Dto;
 import scheduler.app.models.RequestMethod;
+
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @EqualsAndHashCode
 public class RemoteJobEditDto implements Dto {
+
 	private Long remoteJobId;
+
+	@NotBlank(message = "errors.request_url_must_not_be_empty")
 	private String requestUrl;
+
+	@NotNull(message = "errors.request_method_must_not_be_null")
 	private RequestMethod requestMethod;
+
 	private String authString;
+
 	private String postJson;
 
 	@Override
