d69a3bf9f8dcdb3e0e78b77d8356a8bd70343ad3
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-21 16:32:37 +0200

    Integration tests

--- scheduler/src/test/java/scheduler/app/repositories/UserEntityTest.java
+++ scheduler/src/test/java/scheduler/app/repositories/UserEntityTest.java
@@ -9,12 +9,54 @@ import javax.inject.Inject;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserEntityTest extends AbstractRepositoryTest {
 
     @Inject
     private UserRepository userRepository;
+
+    @Test
+    public void shouldSaveUserIfSecurityDetailsAreNull() {
+        UserEntity constructedUser = new UserEntity();
+        constructedUser.setUsername("User name");
+
+        UserEntity createdUserEntity = userRepository.saveAndFlush(constructedUser);
+
+        UserEntity loadedUserEntity = userRepository.findById(createdUserEntity.getId());
+        assertThat(loadedUserEntity, not(is(createdUserEntity)));
+        assertThat(loadedUserEntity.getId(), is(createdUserEntity.getId()));
+
+        UserSecureDetailsEntity loadedUserSecureDetailsEntity = loadedUserEntity.getSecureDetails();
+        assertThat(loadedUserSecureDetailsEntity, is(nullValue()));
+    }
+
+    @Test
+    public void shouldCreateUserAndUpdateThen() {
+        UserEntity constructedUser = new UserEntity();
+        constructedUser.setUsername("User name");
+        UserSecureDetailsEntity constructedUserDetails = new UserSecureDetailsEntity();
+        constructedUserDetails.setLogin("user_login");
+        constructedUserDetails.setRole(UserRole.USER);
+        constructedUserDetails.setPassword("11111111111111111111111111111111111111111111");
+        constructedUserDetails.setUser(constructedUser);
+        constructedUser.setSecureDetails(constructedUserDetails);
+
+        UserEntity createdUserEntity = userRepository.saveAndFlush(constructedUser);
+
+        UserEntity loadedUserEntity = userRepository.findById(createdUserEntity.getId());
+        loadedUserEntity.setUsername("New User Name");
+        UserEntity updatedUserEntity = userRepository.saveAndFlush(loadedUserEntity);
+        assertThat(updatedUserEntity, is(notNullValue()));
+        assertThat(updatedUserEntity.getId(), is(createdUserEntity.getId()));
+        assertThat(updatedUserEntity.getUsername(), is("New User Name"));
+
+        UserSecureDetailsEntity secureDetailsOfUpdatedUser = updatedUserEntity.getSecureDetails();
+        assertThat(secureDetailsOfUpdatedUser, not(is(nullValue())));
+        assertThat(secureDetailsOfUpdatedUser.getId(), is(createdUserEntity.getSecureDetails().getId()));
+    }
 
     @Test
     public void shouldSaveUserAndUserDetailsWhenUserIsSaved() {
@@ -29,14 +71,14 @@ public class UserEntityTest extends AbstractRepositoryTest {
 
         constructedUser.setSecureDetails(constructedUserDetails);
 
-        UserEntity savedUserEntity = userRepository.saveAndFlush(constructedUser);
+        UserEntity createdUserEntity = userRepository.saveAndFlush(constructedUser);
 
-        UserEntity loadedUserEntity = userRepository.findById(savedUserEntity.getId());
-        assertThat(loadedUserEntity, not(is(savedUserEntity)));
-        assertThat(loadedUserEntity.getId(), is(savedUserEntity.getId()));
+        UserEntity loadedUserEntity = userRepository.findById(createdUserEntity.getId());
+        assertThat(loadedUserEntity, not(is(createdUserEntity)));
+        assertThat(loadedUserEntity.getId(), is(createdUserEntity.getId()));
 
         UserSecureDetailsEntity loadedUserSecureDetailsEntity = loadedUserEntity.getSecureDetails();
-        assertThat(loadedUserSecureDetailsEntity, not(is(savedUserEntity.getSecureDetails())));
+        assertThat(loadedUserSecureDetailsEntity, not(is(createdUserEntity.getSecureDetails())));
         assertThat(loadedUserSecureDetailsEntity.getUser(), not(is(constructedUser)));
         assertThat(loadedUserSecureDetailsEntity.getId(), is(loadedUserSecureDetailsEntity.getId()));
         assertThat(loadedUserSecureDetailsEntity.getUser().getId(), is(loadedUserSecureDetailsEntity.getUser().getId()));
