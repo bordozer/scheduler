11515e95e1f24297d82bdb3d56b0be1018d62df5
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-23 17:58:52 +0200

    Quartz Scheduler 7.14

--- scheduler/src/main/java/scheduler/app/services/scheduler/SchedulerJob.java
+++ scheduler/src/main/java/scheduler/app/services/scheduler/SchedulerJob.java
@@ -1,5 +1,6 @@
 package scheduler.app.services.scheduler;
 
+import org.apache.log4j.Logger;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,12 +9,12 @@ import scheduler.app.models.RemoteJob;
 
 public class SchedulerJob extends QuartzJobBean {
 
+    private static final Logger LOGGER = Logger.getLogger(SchedulerJob.class);
+
     @Override
     protected void executeInternal(final JobExecutionContext context) throws JobExecutionException {
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
-
         RemoteJob remoteJob = (RemoteJob) dataMap.get(SchedulerTaskInitializationServiceImpl.SCHEDULER_TASK_REMOTE_JOB);
-
-        System.out.println(String.format("REMOTE JOB: #%d (%s)", remoteJob.getId(), remoteJob.getRequestUrl()));
+        LOGGER.debug(String.format("REMOTE JOB: #%d (%s)", remoteJob.getId(), remoteJob.getRequestUrl()));
     }
 }
