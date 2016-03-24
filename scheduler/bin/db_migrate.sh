e8f3a5d4fc8af49ba976b5d0d6f3e5a6b7c2911e
Borys Lukianov <Borys_Lukianov@epam.com>
2016-03-24 18:23:20 +0200

    Quartz Scheduler 9.1

--- scheduler/bin/db_migrate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-flyway -user=sa -password= -url='jdbc:h2:~/dev/db/h2-db/scheduler;MODE=Oracle;AUTO_SERVER=TRUE' -locations='filesystem:/home/blu/dev/src/scheduler/scheduler/src/main/resources/migration' migrate
